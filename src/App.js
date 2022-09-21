e22579496126e6a79a66de8d1eb8373c5a5c6872
Seutchou <90128041+Seutchou@users.noreply.github.com>
2022-09-21 18:02:53 +0100

    update files
    mind overflow

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import About from "./pages/About";
+import Home from "./pages/Home";
 
 const App = () => {
   return (
-    <div>
-      <h1>Hello world</h1>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/about" element={<About />}></Route>
+        {/* Le path * fonctionne si jamais l'url ne correspond a rien de declare la haut */}
+        <Route path="*" element={<About />}></Route>
+      </Routes>
+    </BrowserRouter>
   );
 };
 
---
+++ src/pages/About.js
@@ -0,0 +1,54 @@
+import React from "react";
+import Logo from "../components/Logo";
+import Navigation from "../components/Navigation";
+
+const About = () => {
+  return (
+    <div>
+      <Logo />
+      <Navigation />
+      <h1>a propos</h1>
+      <br />
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi
+        recusandae eaque corporis molestiae iusto porro consectetur fugiat nisi
+        alias temporibus delectus animi ad quod magnam, odit modi. Quae, nulla?
+        Mollitia, distinctio. Maxime repudiandae sequi, quam quas fugiat
+        necessitatibus, quae recusandae exercitationem esse autem reiciendis
+        tempora hic? Esse temporibus odio voluptates debitis reiciendis, quae
+        vitae a dolorem natus numquam tenetur. Dolores officiis distinctio, sit
+        eius commodi corporis numquam quia nisi. At, id assumenda! Quaerat
+        corrupti obcaecati aspernatur, eveniet pariatur sequi? Vitae alias culpa
+        delectus quam illum, placeat dicta eveniet! Incidunt ratione, dolore
+        dolorem adipisci fuga quaerat tenetur delectus aspernatur odio iste!
+      </p>
+      <br />
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam, commodi!
+        Debitis, ut. Expedita debitis nesciunt quis quibusdam vero, quam culpa
+        fuga, sapiente ut enim odit a. Provident officiis eos ex animi
+        voluptatum, a vero alias quidem ea mollitia rem, eius repellendus
+        aperiam suscipit sit consequuntur nemo nulla error voluptas! Asperiores
+        praesentium est quae provident pariatur maxime soluta libero error cum
+        saepe maiores voluptatibus doloremque sapiente similique cumque rerum
+        delectus deserunt voluptas, tenetur nihil! Laborum aperiam labore,
+        similique unde qui hic pariatur explicabo dignissimos consequuntur
+        sapiente porro, sed ab vel odit neque dolorem animi possimus nihil earum
+        at dicta! Nemo nulla tempora vero eos nostrum nisi odio quibusdam velit
+        id, suscipit illum similique quisquam enim vitae laudantium impedit
+        dignissimos molestiae non magnam optio fuga doloremque. Iste dolor
+        officia sunt aut molestias blanditiis tenetur molestiae, facilis dolore
+        nulla expedita quibusdam accusantium optio beatae, consectetur iure. Aut
+        incidunt alias eaque commodi reprehenderit provident libero.
+        Reprehenderit atque vitae vel possimus rem quas soluta quia mollitia
+        facere laborum. Quod, labore! Repellendus esse quos natus quas velit
+        delectus aliquam, atque odio inventore magni ducimus animi porro
+        necessitatibus suscipit quia soluta ea in, minima error voluptates?
+        Nulla, debitis sed labore odio velit similique possimus illo
+        consectetur! Dicta.
+      </p>
+    </div>
+  );
+};
+
+export default About;
